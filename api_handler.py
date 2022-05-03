5dc36cca3561ec4094ab974393b24f6b84205d82
Hanna Dünschede <dns@ordix.de>
2022-05-03 14:45:23 +0200

    fix nation filter

--- api_handler.py
+++ api_handler.py
@@ -35,6 +35,8 @@ class ApiHandler:
                 continue
             elif type(expression) == list and (all(element.startswith('gender') for element in expression)):
                 self.__unpack_const_list_expression(expression)
+            elif type(expression) == list and (all(element.startswith('nat') for element in expression)):
+                self.__unpack_const_list_expression(expression)
             else:
                 self.__request_api_and_emit(expression)
 
